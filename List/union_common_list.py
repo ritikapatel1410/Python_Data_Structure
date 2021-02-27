c9e20917d30c83d5f2df0a747a9806b474e7ad0c
patelritika1410 <patelrit1410@gmail.com>
2021-02-27 22:15:22 +0530

    Refactor: variable name changed

--- List/union_common_list.py
+++ List/union_common_list.py
@@ -10,18 +10,18 @@ import os
 sys.path.insert(0, os.path.abspath('LogFile'))
 import loggerfile
 
-def common_element(List1,List2):
+def common_element(user_define_list1,user_define_list2):
     """
     Description:
         this function is define for find common items from two lists
     Parameter:
-        List1 (list) : user defined list
-        List2 (list) : use defined list
+        user_define_list1 (list) : user defined list
+        user_define_list2 (list) : use defined list
     Return:
-       List3 (list) : list of common element
+       union_of_list (list) : list of common element
     """
-    List3=list(set(List1).intersection(set(List2)))
-    return List3
+    union_of_list=list(set(user_define_list1).intersection(set(user_define_list2)))
+    return union_of_list
    
 
 def main():
@@ -33,10 +33,10 @@ def main():
     Return:
         None
     """
-    List1=[1,2,3,4,5]
-    List2=[1,2,7,8,3,6]
+    user_define_list1=[1,2,3,4,5]
+    user_define_list2=[1,2,7,8,3,6]
     try:
-        print("=========================================================\n List1 {0} and List2 {1} common element are : {2}".format(List1,List2,common_element(List1,List2)))
+        print("=========================================================\n user_define_list1 {0} and user_define_list2 {1} common element are : {2}".format(user_define_list1,user_define_list2,common_element(user_define_list1,user_define_list2)))
         loggerfile.Logger("info","find common element successfully")
     except Exception as error:
         loggerfile.Logger("error","{0} error occured".format(error))

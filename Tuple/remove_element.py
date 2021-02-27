ab23f01b8c418888c5c14faacaff48fee40934fd
patelritika1410 <patelrit1410@gmail.com>
2021-02-27 14:01:48 +0530

    variable name modified

--- Tuple/remove_element.py
+++ Tuple/remove_element.py
@@ -11,21 +11,23 @@ sys.path.insert(0, os.path.abspath('LogFile'))
 import loggerfile
 from copy import deepcopy
 
-def remove_element(Tuple,element):
+def remove_element(given_tuple,element):
     """
     Description:
         this function is define for remove element from tuple
     Parameter:
-        Tuple (Tuple) : user defined tuple
+        given_tuple (tuple) : user defined tuple
     Return:
-        new_tuple (Tuple) : new tuple after removing element
+        clone_given_tuple (Tuple) : new tuple after removing element
     """
-    remove_tuple=deepcopy(Tuple)
+    remove_tuple=deepcopy(given_tuple)
     remove_tuple=list(remove_tuple)
-    if element in Tuple:
+    if element in given_tuple:
         remove_tuple.remove(element)
-        new_tuple=list(remove_tuple)
-    return new_tuple
+        clone_given_tuple=tuple(remove_tuple)
+        return clone_given_tuple
+    else:
+        return "not found"
 
 
 def main():
@@ -37,10 +39,14 @@ def main():
     Return:
         None
     """
-    Tuple=(1,2,3,4,5,6,7,8,9,10)
+    given_tuple=(1,2,3,4,5,6,7,8,9,10)
     try:
         element=int(input("=======================================================\nenter element: "))
-        print("========================== repeatative element tuple here ==========================\nTuple before : {0}  and after : {1} remove element : {2}".format(Tuple,remove_element(Tuple,element),element))
+        found_element=remove_element(given_tuple,element)
+        if(found_element!="not found"):
+            print("========================== repeatative element tuple here ==========================\nTuple before : {0}  and after : {1} remove element : {2}".format(given_tuple,found_element,element))
+        else:
+            print("===================================================================\nentered element {0} {1} in {2}".format(element,found_element,given_tuple))
         loggerfile.Logger("info","successfully remove element")
     except ValueError as error:
         loggerfile.Logger("error","{0} error occured".format(error))

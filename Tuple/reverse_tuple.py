d0112cba45abf9fce202518f06014ece2f1efd04
patelritika1410 <patelrit1410@gmail.com>
2021-02-27 14:17:03 +0530

    variable name Tuple modified into user_defined_tuple

--- Tuple/reverse_tuple.py
+++ Tuple/reverse_tuple.py
@@ -11,31 +11,31 @@ sys.path.insert(0, os.path.abspath('LogFile'))
 import loggerfile
 from copy import deepcopy
 
-def reverese_Tuple(Tuple):
+def reverese_Tuple(user_defined_tuple):
     """
     Description:
         this function is define for reverse tuple problem
     Parameter:
-        Tuple (Tuple) : user defined tuple
+        user_defined_tuple (tuple) : user defined tuple
     Return:
-        reversed_tuple (Tuple) : reverse tuple of given tuple
+        reversed_tuple (tuple) : reverse tuple of given tuple
     """
-    reversed_tuple=tuple(list(Tuple)[::-1])
+    reversed_tuple=tuple(list(user_defined_tuple)[::-1])
     return reversed_tuple
 
 
 def main():
     """
     Description:
-        this main function for user defind tuple from taking input from user and call rreverse_Tuple
+        this main function for user defind tuple from taking input from user and call reverse_Tuple
     Parameter:
         None
     Return:
         None
     """
-    Tuple=(1,2,3,4,5,6,7,8,9,10)
+    user_defined_tuple=(1,2,3,4,5,6,7,8,9,10)
     try:
-        print("========================== repeatative element tuple here ==========================\nTuple before : {0}  and after : {1} reversed".format(Tuple,reverese_Tuple(Tuple)))
+        print("========================== repeatative element tuple here ==========================\nuser_defined_tuple before : {0}  and after : {1} reversed".format(user_defined_tuple,reverese_Tuple(user_defined_tuple)))
         loggerfile.Logger("info","successfully reversed tuple")
     except ValueError as error:
         loggerfile.Logger("error","{0} error occured".format(error))
